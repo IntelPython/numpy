d7212d872af1dafd0307dc6735d8bdb81da6dc4f
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2019-02-27 08:20:24 -0600

    applied patch conda-recipe/v1.17-patches/intel_umath_optimizations.patch

--- numpy/core/include/numpy/npy_math.h
+++ numpy/core/include/numpy/npy_math.h
@@ -161,8 +161,10 @@ NPY_INPLACE double npy_atan(double x);
 
 NPY_INPLACE double npy_log(double x);
 NPY_INPLACE double npy_log10(double x);
+NPY_INPLACE double npy_erf(double x);
 NPY_INPLACE double npy_exp(double x);
 NPY_INPLACE double npy_sqrt(double x);
+NPY_INPLACE double npy_invsqrt(double x);
 NPY_INPLACE double npy_cbrt(double x);
 
 NPY_INPLACE double npy_fabs(double x);
@@ -271,9 +273,11 @@ NPY_INPLACE float npy_ceilf(float x);
 NPY_INPLACE float npy_rintf(float x);
 NPY_INPLACE float npy_truncf(float x);
 NPY_INPLACE float npy_sqrtf(float x);
+NPY_INPLACE float npy_invsqrtf(float x);
 NPY_INPLACE float npy_cbrtf(float x);
 NPY_INPLACE float npy_log10f(float x);
 NPY_INPLACE float npy_logf(float x);
+NPY_INPLACE float npy_erff(float x);
 NPY_INPLACE float npy_expf(float x);
 NPY_INPLACE float npy_expm1f(float x);
 NPY_INPLACE float npy_asinf(float x);
@@ -314,9 +318,11 @@ NPY_INPLACE npy_longdouble npy_ceill(npy_longdouble x);
 NPY_INPLACE npy_longdouble npy_rintl(npy_longdouble x);
 NPY_INPLACE npy_longdouble npy_truncl(npy_longdouble x);
 NPY_INPLACE npy_longdouble npy_sqrtl(npy_longdouble x);
+NPY_INPLACE npy_longdouble npy_invsqrtl(npy_longdouble x);
 NPY_INPLACE npy_longdouble npy_cbrtl(npy_longdouble x);
 NPY_INPLACE npy_longdouble npy_log10l(npy_longdouble x);
 NPY_INPLACE npy_longdouble npy_logl(npy_longdouble x);
+NPY_INPLACE npy_longdouble npy_erfl(npy_longdouble x);
 NPY_INPLACE npy_longdouble npy_expl(npy_longdouble x);
 NPY_INPLACE npy_longdouble npy_expm1l(npy_longdouble x);
 NPY_INPLACE npy_longdouble npy_asinl(npy_longdouble x);
@@ -464,11 +470,13 @@ static NPY_INLINE npy_longdouble npy_cimagl(npy_clongdouble z)
 double npy_cabs(npy_cdouble z);
 double npy_carg(npy_cdouble z);
 
+npy_cdouble npy_cerf(npy_cdouble z);
 npy_cdouble npy_cexp(npy_cdouble z);
 npy_cdouble npy_clog(npy_cdouble z);
 npy_cdouble npy_cpow(npy_cdouble x, npy_cdouble y);
 
 npy_cdouble npy_csqrt(npy_cdouble z);
+npy_cdouble npy_cinvsqrt(npy_cdouble z);
 
 npy_cdouble npy_ccos(npy_cdouble z);
 npy_cdouble npy_csin(npy_cdouble z);
@@ -492,11 +500,13 @@ npy_cdouble npy_catanh(npy_cdouble z);
 float npy_cabsf(npy_cfloat z);
 float npy_cargf(npy_cfloat z);
 
+npy_cfloat npy_cerff(npy_cfloat z);
 npy_cfloat npy_cexpf(npy_cfloat z);
 npy_cfloat npy_clogf(npy_cfloat z);
 npy_cfloat npy_cpowf(npy_cfloat x, npy_cfloat y);
 
 npy_cfloat npy_csqrtf(npy_cfloat z);
+npy_cfloat npy_cinvsqrtf(npy_cfloat z);
 
 npy_cfloat npy_ccosf(npy_cfloat z);
 npy_cfloat npy_csinf(npy_cfloat z);
@@ -521,11 +531,13 @@ npy_cfloat npy_catanhf(npy_cfloat z);
 npy_longdouble npy_cabsl(npy_clongdouble z);
 npy_longdouble npy_cargl(npy_clongdouble z);
 
+npy_clongdouble npy_cerfl(npy_clongdouble z);
 npy_clongdouble npy_cexpl(npy_clongdouble z);
 npy_clongdouble npy_clogl(npy_clongdouble z);
 npy_clongdouble npy_cpowl(npy_clongdouble x, npy_clongdouble y);
 
 npy_clongdouble npy_csqrtl(npy_clongdouble z);
+npy_clongdouble npy_cinvsqrtl(npy_clongdouble z);
 
 npy_clongdouble npy_ccosl(npy_clongdouble z);
 npy_clongdouble npy_csinl(npy_clongdouble z);
