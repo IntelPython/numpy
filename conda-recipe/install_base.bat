09230f2b5ec75cc37be213c587ba1022b1b77c26
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2019-07-18 13:05:31 -0500

    WIP on recipes

---
+++ conda-recipe/install_base.bat
@@ -0,0 +1,30 @@
+
+call %RECIPE_DIR%\activate_compiler.bat
+if errorlevel 1 exit 1
+set CC=icl
+set LD=xilink
+
+echo "Executing %PYTHON% %RECIPE_DIR%\cio_files\mkl_version.py numpy\__init__.py"
+%PYTHON% %RECIPE_DIR%\cio_files\mkl_version.py numpy\__init__.py
+if errorlevel 1 exit 1
+
+echo "... done"
+
+copy %RECIPE_DIR%\cio_files\numpy\site-mkl-%SUBDIR%.cfg site.cfg
+if errorlevel 1 exit 1
+
+sed -i 's,libs =.*,libs = mkl_rt,g' site.cfg
+@rem sed -i 's#libs =.*#libs = mkl_intel_lp64_dll, mkl_intel_thread_dll, mkl_core_dll, libiomp5md#g' site.cfg
+if errorlevel 1 exit 1
+
+
+%PYTHON% %RECIPE_DIR%\cio_files\replace.py @PREFIX@ "%PREFIX%" site.cfg
+if errorlevel 1 exit 1
+
+set CFLAGS=-DNDEBUG -I%PREFIX%\Library\include %CFLAGS%
+%PYTHON% setup.py config_cc --compiler=intelemw build --force install --old-and-unmanageable
+if errorlevel 1 exit 1
+%PYTHON% setup.py bdist_wheel
+if errorlevel 1 exit 1
+copy dist\numpy-1*.whl ..\..\
+if errorlevel 1 exit 1
---
+++ conda-recipe/install_base.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [ $NOMKL == 1 ]
+then # Non-MKL
+    if [ `uname` == Linux ]; then
+        SITE_CFG="site-openblas-linux.cfg"
+    else
+        SITE_CFG="site-empty.cfg"
+    fi
+else # MKL
+    $PYTHON $RECIPE_DIR/cio_files/mkl_version.py ./numpy/__init__.py
+    if [ `uname` == Darwin ]; then
+        export ATLAS=1
+        export LDFLAGS="-headerpad_max_install_names $LDFLAGS"
+    fi
+    SITE_CFG="site-mkl-$SUBDIR.cfg"
+fi
+
+sed -e "s,@PREFIX@,$PREFIX," <$RECIPE_DIR/cio_files/numpy/$SITE_CFG >site.cfg
+
+if [ `uname` == Darwin ]; then
+    source /localdisk/tools/intel/compilers_and_libraries_16.0.3/mac/bin/compilervars.sh intel64
+    sed -i -e 's/mkl_libs =.*/mkl_libs = mkl_rt/g' site.cfg
+    sed -i -e 's/lapack_libs =.*/lapack_libs = mkl_rt/g' site.cfg
+    export MACOSX_DEPLOYMENT_TARGET=10.10
+else
+    # source /localdisk/tools/intel/compilers_and_libraries_16.0.3/linux/bin/compilervars.sh intel64
+    sed -i 's,libs =.*,libs = mkl_rt,g' site.cfg
+fi
+
+if [ ! -f site.cfg ]; then
+    echo "ERROR: *** site.cfg missing ***"
+    exit 1
+fi
+
+CFLAGS="-DNDEBUG -I$PREFIX/include $CFLAGS" $PYTHON setup.py config_cc --compiler=intelem config_fc --fcompiler=intelem build --force install --old-and-unmanageable
+if [ `uname` == Linux ]; then
+    intel-libpatch.sh
+fi
+$PYTHON setup.py bdist_wheel
+cp dist/numpy*.whl ../../
