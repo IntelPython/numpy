a137cb4ccbd70dcda0950617a20d720ad71f314c
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2019-07-18 13:05:31 -0500

    meta.yaml with multi-oputput building numpy, mkl_fft and mkl_random, consistent with the recipe used by Anaconda

---
+++ conda-recipe/install_mkl_fft.bat
@@ -0,0 +1,26 @@
+pushd mkl_fft
+if %PY_VER%==2.7 (
+    set VS=vs2008
+    set ICC_COMP_BAT="C:\Program Files (x86)\Intel\Composer XE 2013 SP1\bin\compilervars_arch.bat"
+) else (
+    set VS=vs2015
+    set ICC_COMP_BAT="C:\Program Files (x86)\IntelSWTools\parallel_studio_xe_2016.3.059\compilers_and_libraries_2016\windows\bin\compilervars_arch.bat"
+)
+
+call %ICC_COMP_BAT% intel64 %VS%
+if errorlevel 1 exit 1
+set CC=icl
+set LD=xilink
+
+
+set CFLAGS=-I%PREFIX%\Library\include %CFLAGS%
+set LDFLAGS=/LIBPATH:%PREFIX%\Library\lib %LDFLAGS%
+
+%PYTHON% setup.py build --force --compiler=intelemw
+%PYTHON% setup.py install --old-and-unmanageable
+if errorlevel 1 exit 1
+%PYTHON% setup.py bdist_wheel
+if errorlevel 1 exit 1
+copy dist\mkl_fft*.whl ..\..\
+if errorlevel 1 exit 1
+popd
